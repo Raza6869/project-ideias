9cee7e77039f63f54fade195b16f5ca559bea75c
Rafael Thumaza <rassraza4@gmail.com>
2023-06-14 12:15:31 -0300

    Update 14 June
    Sign up Validation

--- to-do-list/Cadastro/cadastro.js
+++ to-do-list/Cadastro/cadastro.js
@@ -1,18 +1,57 @@
-const fname = document.querySelector("input#firstname");
-const sname = document.querySelector("input#secondname");
-const mail = document.querySelector("input#acessmail");
-const username = document.querySelector("input#username");
-const password = document.querySelector("input#password");
-const passwordConf = document.querySelector("input#passwordconfirm");
-const avatarImg = document.querySelector("img#avatar");
-const submit = document.querySelector("button.submit")
+const form = document.getElementById('form');
+const campos = document.querySelectorAll('.required');
+const spans = document.querySelectorAll(".spans-required");
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-const client = {
-    firstName: fname.value,
-    secondName: sname.value,
-    email: mail.value,
-    username: username.value,
-    password: password.value,
-    avatar: avatarImg.value,
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    nameValidate();
+    emailValidate();
+    passwordValidate();
+    comparePassword();
+});
+
+
+function setError(index){
+    campos[index].style.border = "2px solid red";
+    spans[index].style.display = 'block';
 }
+function removeError(index){
+    campos[index].style.border = '0';
+    spans[index].style.display = 'none';
+}
+
+function nameValidate(){
+    if(campos[0].value.length < 3){
+        setError(0);
+    }else{
+        removeError(0);
+    }
+}
+
+function emailValidate(){
+    if(!emailRegex.test(campos[1].value)){
+        setError(1);
+    }else{
+        removeError(1);
+    }
+}
+
+function passwordValidate(){
+    if(campos[2].value.length < 8){
+        setError(2);
+    }else{
+        removeError(2);
+        comparePassword()
+    }
+}
+
+function comparePassword(){
+    if(campos[2].value == campos[3].value){
+        removeError(3);
+    }else{
+        setError(3);
+    }
+}
+
 
